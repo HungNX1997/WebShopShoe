3f9c27597c7071ed93d8199de7bbfa1ebd23a7ca
HungNX <HungNX@DESKTOP-IIPHGHE>
2019-06-08 11:04:57 +0700

    update Home controller

--- WebShopShoe/Controllers/HomeController.cs
+++ WebShopShoe/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebShopShoe.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "WebShopShoe";
 
             return View();
         }
